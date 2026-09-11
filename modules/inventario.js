5edd6bff5cd0589b37f6ed7aa14c7f2811b639ee
panaderiamarlu-boop <panaderiamarlu@gmail.com>
2026-09-11 15:42:54 -0500

    Update inventario.js

--- modules/inventario.js
+++ modules/inventario.js
@@ -1,1 +1,64 @@
+function cargarInventario(productos){
 
+
+let tabla = "";
+
+
+productos.forEach(producto=>{
+
+
+let stock = Number(producto["STOCK ACTUAL"]);
+
+let minimo = Number(producto["STOCK MINIMO"]);
+
+
+let estado = "";
+
+
+
+if(stock <= minimo){
+
+estado = "🔴 Bajo";
+
+}
+
+else if(stock <= minimo * 1.5){
+
+estado = "🟡 Atención";
+
+}
+
+else{
+
+estado = "🟢 OK";
+
+}
+
+
+
+tabla += `
+
+<tr>
+
+<td>${producto.NOMBRE}</td>
+
+<td>${producto.CATEGORIA}</td>
+
+<td>${stock} ${producto.UNIDAD}</td>
+
+<td>${minimo}</td>
+
+<td>${estado}</td>
+
+</tr>
+
+`;
+
+
+});
+
+
+document.getElementById("productos").innerHTML = tabla;
+
+
+}
