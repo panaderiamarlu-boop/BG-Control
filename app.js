bcd73f7c8568897dfd3219dd478df9f7644a17a4
panaderiamarlu-boop <panaderiamarlu@gmail.com>
2026-09-11 15:46:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
-const API = "https://bg-control-six.vercel.app";
+const API = "https://script.google.com/macros/s/AKfycbzRo0LE3qL2Ys7VSegBxvMtls4IjiCyOkymYkrjgxokWlgKWcUpuQ85pUrLM0MuQpSoiw/exec";
 
 
-fetch(API+"?accion=productos")
+
+fetch(API + "?accion=productos")
 
 
-.then(res=>res.json())
+.then(res => res.json())
 
 
-.then(data=>{
+.then(data => {
 
 
-document.getElementById("totalProductos").innerHTML=data.length;
+console.log(data);
 
 
-let alertas=0;
-let valor=0;
 
-let filas="";
+document.getElementById("totalProductos").innerHTML = data.length;
+
+
+
+let alertas = 0;
+
+let valor = 0;
+
 
 
 data.forEach(producto=>{
 
 
-let stock=Number(producto["STOCK ACTUAL"]);
+let stock = Number(producto["STOCK ACTUAL"]);
 
-let minimo=Number(producto["STOCK MINIMO"]);
+let minimo = Number(producto["STOCK MINIMO"]);
 
-let costo=Number(producto["COSTO UNITARIO"]);
-
-
-
-valor += stock*costo;
+let costo = Number(producto["COSTO UNITARIO"]);
 
 
 
-if(stock<=minimo){
+valor += stock * costo;
+
+
+
+if(stock <= minimo){
 
 alertas++;
 
 }
 
 
-
-filas+=`
-
-<tr>
-
-<td>${producto.NOMBRE}</td>
-
-<td>${producto.CATEGORIA}</td>
-
-<td>${stock} ${producto.UNIDAD}</td>
-
-<td>${minimo}</td>
-
-</tr>
-
-`;
-
-
 });
 
 
 
-document.getElementById("totalAlertas").innerHTML=alertas;
-
-
-document.getElementById("valorInventario").innerHTML=
-
-"$ "+valor.toLocaleString("es-CO");
+document.getElementById("totalAlertas").innerHTML = alertas;
 
 
 
-document.getElementById("productos").innerHTML=filas;
+document.getElementById("valorInventario").innerHTML =
+
+"$ " + valor.toLocaleString("es-CO");
+
+
+
+cargarInventario(data);
+
+
+
+})
+
+
+.catch(error=>{
+
+
+console.error(
+"Error conectando API:",
+error
+);
 
 
 });
