842e1c08bfc1d6fb6b31a894728724149f10201c
panaderiamarlu-boop <panaderiamarlu@gmail.com>
2026-09-16 16:01:25 -0500

    Update google.js

--- api/google.js
+++ api/google.js
@@ -1,60 +1,9 @@
-/*
-=====================================
- BG CONTROL
- VERCEL API BRIDGE
-=====================================
-*/
+export default function handler(req, res) {
 
+    res.status(200).json({
 
-export default async function handler(req, res) {
+        estado:"Vercel API funcionando"
 
-
-    const GOOGLE_API =
-    "https://script.google.com/macros/s/AKfycbw0Gb-78mX0V-yUKCks6cSowz8F-tBTte9_Ppvr5tuCBGykBe4qNfFB3dQuGcmb2mbQNw/exec";
-
-
-
-    try {
-
-
-        const accion =
-        req.query.accion || "productos";
-
-
-
-        const respuesta =
-        await fetch(
-            GOOGLE_API +
-            "?accion=" +
-            encodeURIComponent(accion)
-        );
-
-
-
-        const datos =
-        await respuesta.json();
-
-
-
-        // DEVUELVE DIRECTAMENTE EL ARRAY
-        res.status(200).json(datos);
-
-
-
-    } catch(error){
-
-
-        res.status(500).json({
-
-            error:error.message
-
-        });
-
-
-    }
-
-
-}
-
+    });
 
 }
